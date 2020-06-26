c96ac4c16f0a3c5d7e696f65d59b4167106b34cb
Naziakhatoon <naziafirdous7@gmail.com>
2020-06-26 23:04:30 +0530

    student file modified again

--- Student1.java
+++ Student1.java
@@ -19,9 +19,11 @@ class Student1
     public static void main(String args[]){  
     Student1 s1 = new Student1(111,"Nazia",26);  
     Student1 s2 = new Student1(222,"Ram",25); 
-    Student1 s3 = new Student1(333,"Shyam",30);  
+    Student1 s3 = new Student1(333,"Shyam",30); 
+    Student1 s4 = new Student1(333,"Gita",32);   
     s1.display();  
     s2.display();  
     s3.display(); 
+    s4.display(); 
    }  
 }
